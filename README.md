45a7052bac80edc996123a8fa315f5207a83311f
vaggelisSp <104629645+vaggelisSp@users.noreply.github.com>
2022-04-29 14:47:24 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# proggrame2.github.io
---
+++ script.js
@@ -0,0 +1,41 @@
+function dark_mode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+function white_mode() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+}
+
+function red_mode() {
+    var element = document.body;
+    element.classList.toggle("red-mode");
+}
+
+function yellow_mode() {
+    var element = document.body;
+    element.classList.toggle("yellow-mode");
+}
+
+function purple_mode() {
+    var element = document.body;
+    element.classList.toggle("purple-mode");
+}
+
+function girls_mode() {
+    var element = document.body;
+    element.classList.toggle("girlsBackground");
+}
+function girls_mode2() {
+    var element = document.body;
+    element.classList.toggle("girlsBackground2");
+}
+function gaming_mode() {
+    var element = document.body;
+    element.classList.toggle("gamingBackground");
+}
+function gaming_mode2() {
+    var element = document.body;
+    element.classList.toggle("gamingBackground");
+}
